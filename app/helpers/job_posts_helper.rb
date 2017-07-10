b166cacd87494bf55dc88fe353b9759d3e83edd8
mohan007 <mohan589@gmail.com>
2017-07-10 13:29:04 +0000

    changes for layout

--- app/helpers/job_posts_helper.rb
+++ app/helpers/job_posts_helper.rb
@@ -2,4 +2,8 @@ module JobPostsHelper
   def is_post_user_current_user_same(job)
     job.user_id == current_user.id if current_user.present?
   end
+  
+  def if_current_user_is_recruiter
+    current_user&.user_type_id == 2
+  end
 end
